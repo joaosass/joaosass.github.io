4c15cedb9fa8fe202797ef71d345ca70c6ffa28b
JHSS95 <j_henrique95@outlook.com>
2016-06-15 02:36:10 -0300

    Modificações

--- js/script.js
+++ js/script.js
@@ -1,85 +1,14 @@
-var ind = 0;
+var nav = document.querySelector("nav");
+var botao = document.querySelector(".botao");
+var main = document.querySelector("main");
 var li = document.querySelectorAll("li");
 for(var i = 0; i < 4; i++){
     li[i].onclick = function(){
-        nav.style.left = "-100vh";
-        botao.style.left = "5vh";
-        ind = 0;
-        if(this.innerHTML == "Inicio"){
-            var tempo = setInterval(function(){
-                if(window.pageYOffset != 0){
-                    window.scrollTo(0, window.pageYOffset - 90);
-                }
-                else{
-                    clearInterval(tempo);
-                }
-            },0);
-        }
-        else if(this.innerHTML == "Perfil" && window.pageYOffset < document.querySelector("#perfil").offsetTop){
-            var tempo = setInterval(function(){
-                if(window.pageYOffset < document.querySelector("#perfil").offsetTop){
-                    window.scrollTo(0, window.pageYOffset + 60);
-                }
-                else{
-                    clearInterval(tempo);
-                }
-            },0);
-        }
-        else if(this.innerHTML == "Perfil" && window.pageYOffset > document.querySelector("#perfil").offsetTop){
-            var tempo = setInterval(function(){
-                if(window.pageYOffset > document.querySelector("#perfil").offsetTop){
-                    window.scrollTo(0, window.pageYOffset - 60);
-                }
-                else{
-                    clearInterval(tempo);
-                }
-            },0);
-        }
-        else if(this.innerHTML == "Habilidades" && window.pageYOffset < document.querySelector("#linguagens").offsetTop){
-            var tempo = setInterval(function(){
-                if(window.pageYOffset < document.querySelector("#linguagens").offsetTop){
-                    window.scrollTo(0, window.pageYOffset + 60);
-                }
-                else{
-                    clearInterval(tempo);
-                }
-            },0);
-        }
-        else if(this.innerHTML == "Habilidades" && window.pageYOffset > document.querySelector("#linguagens").offsetTop){
-            var tempo = setInterval(function(){
-                if(window.pageYOffset > document.querySelector("#linguagens").offsetTop){
-                    window.scrollTo(0, window.pageYOffset - 60);
-                }
-                else{
-                    clearInterval(tempo);
-                }
-            },0);
-        }
-        else if(this.innerHTML == "Portfólio"){
-            var tempo = setInterval(function(){
-                if(window.pageYOffset <= document.querySelector("#portfolio").offsetTop){
-                    window.scrollTo(0, window.pageYOffset + 90);
-                }
-                else{
-                    clearInterval(tempo);
-                }
-            },0);
-        }
-        return false;
+        nav.classList.toggle("mostra");
+        main.classList.toggle("mostra");
     }
 }
-
-var nav = document.querySelector("nav");
-var botao = document.querySelector(".botao");
 botao.onclick = function(){
-    if(ind == 0){
-        nav.style.left = 0;
-        botao.style.left = "225px";
-        ind = 1;
-    }
-    else{
-        nav.style.left = "-100vh";
-        botao.style.left = "5vh";
-        ind = 0;
-    }
+    nav.classList.toggle("mostra");
+    main.classList.toggle("mostra");
 }
